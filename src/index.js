b49f550a30afa3aabe9a0f384c150b3873e87f0b
A3thyr <ennjaletgo@yandex.ru>
2023-10-05 21:58:26 +0300

    feat: added ham menu

--- src/index.js
+++ src/index.js
@@ -1,1 +1,23 @@
 import './styles.scss'
+
+const menu = document.querySelector(".header-nav-list")
+const menuBtn = document.querySelector(".header-nav-logbtn-burgerbtn")
+const menuBtnClose = document.querySelector(".header-nav-list-buttons-exit")
+
+if (menu && menuBtn && menuBtnClose){
+    menuBtn.addEventListener('click', () =>{
+        menu.classList.toggle('active')
+        menuBtn.classList.toggle('active')
+    })
+    menuBtnClose.addEventListener('click', () =>{
+        menu.classList.toggle('active')
+        menuBtnClose.classList.toggle('active')
+    })
+
+    menu.querySelectorAll('a').forEach(link => {
+        link.addEventListener('click', () => {
+            menu.classList.toggle('active')
+            menuBtnClose.classList.toggle('active')
+        })
+    })
+}
